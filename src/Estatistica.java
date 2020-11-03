a62d19fe9e25763419ae0fe77879e95e824520f7
Miguel <miguellimatavares@gmail.com>
2020-11-03 10:44:29 -0300

    Classe estatistica adicionada

---
+++ src/Estatistica.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public abstract class Estatistica {
+	
+	private String nome;
+	private List<Medicao> observacoes;
+	
+	
+	public void inclui(Medicao observacao) {
+		
+	}
+	
+	public LocalDate dataInicio() {
+		
+	}
+	
+	public LocalDate dataFim() {
+		
+	}
+	
+	public float valor() {
+		
+	}
+}
