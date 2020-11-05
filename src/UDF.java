d6d161fc680c98b11a0165e4197fd2c2710fe7d1
João Henrique Schmidt <jhsc98@gmail.com>
2020-11-05 17:04:25 -0300

    Adicionado lista de funções

---
+++ src/UDF.java
@@ -0,0 +1,66 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * Utilitarios e funções definidas pelo usuário (UDF)
+ * É mais útil ler outros códigos por ai do que explorar 
+ * mais desse arquivo. As funções são feitas para serem 
+ * usadas de forma independente uma das outras.
+ * @author JoÃ£o Henrique Schmidt
+ *
+ */
+public class UDF {
+	/**
+	 * Retira o último character da string
+	 * @param s String desejada
+	 * @return String final
+	 */
+	public static String chop(String s) {
+		if(s.length() > 0) {
+			return s.substring(0,s.length() -1);
+		}else {
+			return s;
+		}
+	}
+	/**
+	 * Testa se String pode ser número.
+	 * @param s string que deve representar um número
+	 * @return true caso paramtro seja verdadeiro. Senão falso.
+	 */
+	public static boolean isNumber(String s) {
+		if(s.matches("\\d*")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	/**
+	 * Testa se a data informada é válida
+	 * @param s Data em forma de string
+	 * @return true caso a data seja verdadeira. Senão false
+	 */
+	public static boolean isDateValid(String s) {
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+		df.setLenient(false); //impede datas absurdas e.g MM = 20;
+		try {
+			df.parse(s);
+		} catch (ParseException e) {
+			return false;
+		}
+		return true;
+	}
+	/**
+	 * Testa se String pode ser float.
+	 * @param s string que deve representar um número
+	 * @return true caso paramtro seja verdadeiro. Senão falso.
+	 */
+	public static boolean isFloat(String s) {
+		try {
+			Float.parseFloat(s);
+		}catch (NumberFormatException | NullPointerException e) {
+			return false;
+		}
+		return true;
+	}
+}
